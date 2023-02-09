dafef5fe1a5144a816e4c347b1b1d2ae186f968b
FilleDeZeus <enide26@hotmail.fr>
2023-02-09 11:45:04 +0100

    fin

--- src/compenents/bouton/bouton.js
+++ src/compenents/bouton/bouton.js
@@ -1,10 +1,14 @@
 import React, {useState} from 'react'
 import './public/css/bouton.css'
+
+//FONCTION BOUTON
 export const Bouton = () => {
+    //Tableau
     const [compteur, setCount] = useState(0);
 
     return (
         <div>
+            {/* BOUTON QUI PERMET AU CLICK D'APPELER LA FONCTION */}
             <button onClick={() => setCount(compteur + 1)}>Click Me</button>
             <p>Compteur: {compteur}</p>
         </div>
