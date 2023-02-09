b0fb7c2f4696f9e0884ffc400f40a20990cc6c58
FilleDeZeus <enide26@hotmail.fr>
2023-02-09 11:24:09 +0100

    fin

--- src/compenents/bouton/bouton.js
+++ src/compenents/bouton/bouton.js
@@ -1,12 +1,12 @@
 import React, {useState} from 'react'
 import './public/css/bouton.css'
 export const Bouton = () => {
-    const [count, setCount] = useState(0);
+    const [compteur, setCount] = useState(0);
 
     return (
         <div>
-            <button onClick={() => setCount(count + 1)}>Click Me</button>
-            <p>Count: {count}</p>
+            <button onClick={() => setCount(compteur + 1)}>Click Me</button>
+            <p>Compteur: {compteur}</p>
         </div>
     );
 }
