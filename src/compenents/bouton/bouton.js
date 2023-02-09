7783a44bce51b31752cd4228e070ceedda23073d
FilleDeZeus <enide26@hotmail.fr>
2023-02-09 13:25:34 +0100

    fin

--- src/compenents/bouton/bouton.js
+++ src/compenents/bouton/bouton.js
@@ -4,12 +4,12 @@ import './public/css/bouton.css'
 //FONCTION BOUTON
 export const Bouton = () => {
     //Tableau
-    const [compteur, setCount] = useState(0);
+    const [compteur, setCompteur] = useState(0);
 
     return (
         <div>
             {/* BOUTON QUI PERMET AU CLICK D'APPELER LA FONCTION */}
-            <button onClick={() => setCount(compteur + 1)}>Click Me</button>
+            <button onClick={() => setCompteur(compteur + 1)}>Click Me</button>
             <p>Compteur: {compteur}</p>
         </div>
     );
